5e34470d463e736d5099da7a2f3ef46c7675ba5c
hritikdangi3 <hritikdangi3@gmail.com>
2022-03-17 18:29:26 +0530

    sumZero with naiva approach

--- sumZero.js
+++ sumZero.js
@@ -4,6 +4,18 @@
 //for exmaple : [-3,-2,-1,0,1,2,3] ans-> [-3,3] bec their sum results in 0
 // example: [-2,0,1,3]  and-> undefined bec -2+3= 1 which != 0
 
+// This is the naive approach gives the O(n^2)
 function sumZero(arr){
-    
+    // we will interate over first index
+    for(let i=0;i<arr.length; i++){
+        // meanwhile checks the rest array(thatswhy we did j=i+1) that any element in array matches with first index which gives addition to zero
+        for(let j=i+1;j< arr.length; j++){
+            //if sum results in zer0 -3+3=0
+            if(arr[i] + arr[j] ===0){
+                // return that element [-3,3]
+                return (arr[i], arr[j])
+            }
+        }
+    }
 }
+sumZero([-3,-2,-1,0,1,2,3])
